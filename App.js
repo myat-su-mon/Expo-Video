a71b515cf0ae804cae74d4770376ac36237eeb0d
msm <myatsumon@ucsy.edu.mm>
2023-06-13 16:10:02 +0630

    expo video testing

--- App.js
+++ App.js
@@ -1,11 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Video } from "expo-av";
+import { StatusBar } from "expo-status-bar";
+import { useRef, useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const video = useRef(null);
+  const secondVideo = useRef(null);
+  const [status, setStatus] = useState({});
+  const [statusSecondVideo, setStatusSecondVideo] = useState({});
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Video
+        ref={video}
+        style={styles.video}
+        source={{
+          uri: "https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+        }}
+        useNativeControls
+        resizeMode="contain"
+        isLooping
+        onPlaybackStatusUpdate={setStatus}
+      />
+      <View style={styles.buttons}>
+        <Button
+          title="Play from 5s"
+          onPress={() => video.current.playFromPositionAsync(5000)}
+        />
+        <Button
+          title={status.isLooping ? "Set to not loop" : "Set to loop"}
+          onPress={() => video.current.setIsLoopingAsync(!status.isLooping)}
+        />
+      </View>
+      <Video
+        ref={secondVideo}
+        style={styles.video}
+        source={{
+          uri: "https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+        }}
+        useNativeControls
+        resizeMode="contain"
+        isLooping
+        onPlaybackStatusUpdate={setStatusSecondVideo}
+      />
+      <View style={styles.buttons}>
+        <Button
+          title="Play from 50s"
+          onPress={() => secondVideo.current.playFromPositionAsync(50000)}
+        />
+        <Button
+          title={
+            statusSecondVideo.isLooping ? "Set to not loop" : "Set to loop"
+          }
+          onPress={() =>
+            secondVideo.current.setIsLoopingAsync(!statusSecondVideo.isLooping)
+          }
+        />
+      </View>
     </View>
   );
 }
@@ -13,8 +64,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  video: {
+    flex: 1,
+    alignSelf: "stretch",
+  },
+  buttons: {
+    gap: 10,
   },
 });
